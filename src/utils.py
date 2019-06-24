661f9d4c91eace298d58222e6ab2523b29219696
Ahmad <ahmad.baa003@gmail.com>
2019-06-24 17:59:15 +0200

    Removing useless comments

--- src/utils.py
+++ src/utils.py
@@ -9,29 +9,15 @@ def random_style():
         return tf.read()
 
 def get_random_word():
-    """
-        Returns a random word from the list in the
-        data/words.json file
-    """
     with open('data/words.json', 'r') as wf:
         words = json.loads(wf.read())
         return random.choice(words).upper()
 
 def register_scores(scores):
-    """
-        Writes the list of scores entered in the parameters
-        to the datas/scores.json file
-    """
     with open('data/scores.json', 'w') as sf:
         sf.write(json.dumps(scores))
 
 def init_scores():
-    """
-        Initializes the scores dictionnary, the username
-        entered by the user and his score in the datas/scores.json file
-
-        scores = {username: score, ...}
-    """
     scores = {}
     if os.path.isfile('data/scores.json'):
         with open('data/scores.json', 'r') as sf:
@@ -54,10 +40,6 @@ def init_scores():
     return (username, score, scores)
 
 def get_drawings():
-    """
-        Returns the list of all the drawings
-        for the Hangman game located in the datas/drawings.json file
-    """
     with open('data/drawings.json', 'r') as df:
         drawings = json.loads(df.read())
         return drawings
