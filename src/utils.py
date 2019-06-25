6903747c537702de2bd03eac8c64022f14c08f20
Ahmad <ahmad.baa003@gmail.com>
2019-06-25 15:11:28 +0200

    Removing useless functions

--- src/utils.py
+++ src/utils.py
@@ -8,38 +8,13 @@ def random_style():
     with open(path+str(n), 'r') as tf:
         return tf.read()
 
-def get_random_word():
-    with open('data/words.json', 'r') as wf:
+def random_word():
+    path = '/home/ahmad/workspace/Hangman/src/data/words.json'
+    with open(path, 'r') as wf:
         words = json.loads(wf.read())
         return random.choice(words).upper()
 
-def register_scores(scores):
-    with open('data/scores.json', 'w') as sf:
-        sf.write(json.dumps(scores))
-
-def init_scores():
-    scores = {}
-    if os.path.isfile('data/scores.json'):
-        with open('data/scores.json', 'r') as sf:
-            scores = json.loads(sf.read())
-
-    username = ' '
-    score = 0
-
-    while ' ' in username:
-        username = input('Enter your username : ')
-        if ' ' in username:
-            print('You can not put a space in your username')
-
-    if username in scores.keys():
-        score = scores[username]
-        print(f'Welcome back {username}')
-    else:
-        print(f'Welcome {username}')
-
-    return (username, score, scores)
-
-def get_drawings():
-    with open('data/drawings.json', 'r') as df:
-        drawings = json.loads(df.read())
-        return drawings
+def drawings():
+    path = '/home/ahmad/workspace/Hangman/src/data/drawings.json'
+    with open(path, 'r') as df:
+        return '\n'.join(json.loads(df.read()))
