f4368c7ba30b14e33be6f133a0464e60f8e903c1
Ahmad <ahmad.baa003@gmail.com>
2019-06-26 00:49:29 +0200

    Widgets and menus code reorganization

--- src/view.py
+++ src/view.py
@@ -8,34 +8,80 @@ palette = [
 
 title = urwid.Filler(urwid.Text(utils.random_style(), align='center'))
 
-t_player = urwid.Text('')
-t_score = urwid.Text('')
+b_game_ok = urwid.Button('OK')
+e_word = urwid.Edit(('ask', 'Enter a letter:\n> '))
+
+def s_game(display, player, drawing, wrongs):
+    scores = urwid.LineBox(
+        urwid.Pile([
+            ('pack', urwid.Text(f'Username: {player.username}.')),
+            ('pack', urwid.Text(f'Score: {player.score}.'))]),
+        title='Player')
+
+    current = urwid.LineBox(
+        urwid.Frame(
+            urwid.Filler(urwid.Text(drawing, align='center')),
+            urwid.Text(', '.join(wrongs))))
+
+    input_letter = urwid.LineBox(
+        urwid.Pile([
+            ('pack', e_word),
+            ('pack', b_game_ok)]))
+
+    word = urwid.Filler(
+        urwid.Pile([
+            ('pack', urwid.Text(('ask', 'Word:\n'))),
+            ('pack', urwid.Text(display))]))
+
+    piles_left = urwid.Pile([
+        (36, word),
+        ('weight', 36, input_letter)])
+
+    piles_right = urwid.Pile([
+        (20, scores),
+        (50, current)])
+
+    columns = urwid.Columns([
+        (35, piles_left),
+        (35, piles_right)])
+
+    return urwid.Overlay(columns, urwid.SolidFill(' '),
+        align='center', width=('relative', 75),
+        valign='middle', height=('relative', 75))
+
 b_start = urwid.Button('Start!')
 
 def m_home(username, score, new):
-    text = ''
+    raw = ''
     if new == True:
-        text = 'Looks like you are new here.'
+        raw = 'Looks like you are new here.'
     else:
-        text = 'Well, you already played this game, welcome back!'
-    text += f'\nTherefore, your current score is : {score}'
+        raw = 'Well, you already played this game, welcome back!'
+    raw += f'\nTherefore, your current score is : {score}'
+
+    title = urwid.Text(('ask', f'Welcome, {username}.'), align='center')
+    text = urwid.Filler(urwid.Text(raw))
+    button = urwid.Filler(
+        urwid.AttrMap(b_start, None, focus_map='reversed'),
+        valign='bottom')
+
+    pile = urwid.Pile([
+        ('pack', urwid.Divider()),
+        ('pack', title),
+        ('weight', 5, text),
+        ('weight', 2, button)])
 
     return urwid.Overlay(
-        urwid.LineBox(
-            urwid.Pile([
-                ('pack', urwid.Divider()),
-                ('pack', urwid.Text(('ask', f'Welcome, {username}.'), align='center')),
-                ('weight', 5, urwid.Filler(urwid.Text(text))),
-                ('weight', 2, urwid.Filler(
-                    urwid.AttrMap(b_start, None, focus_map='reversed'),
-                    valign='bottom'))]),
-            title='Hi!'),
+        urwid.LineBox(pile, title='Hi!'),
         urwid.SolidFill(' '),
         align='center', width=('relative', 50),
         valign='middle', height=('relative', 50))
 
 b_enter = urwid.Button('Enter')
 e_username = urwid.Edit(('ask', 'Enter your name:\n> '))
+
+def m_login():
+    pass
 
 m_login = urwid.Overlay(
     urwid.LineBox(
@@ -52,6 +98,9 @@ m_login = urwid.Overlay(
 
 b_game = urwid.Button('Enter Game')
 b_quit = urwid.Button('Quit')
+
+def m_welcome():
+    pass
 
 m_welcome = urwid.Overlay(
     urwid.LineBox(
