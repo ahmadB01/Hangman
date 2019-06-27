a64a85a2e58e836c502b371c24a6b461388a5ecf
Ahmad <ahmad.baa003@gmail.com>
2019-06-27 23:40:20 +0200

    Changing event functions system + Added new window

--- src/controller.py
+++ src/controller.py
@@ -3,45 +3,58 @@ import view
 import model
 import utils
 
+def eventhandler(button):
+    def bind(func):
+        urwid.connect_signal(button, 'click', func)
+        return func
+    return bind
+
 def main():
-    register_events()
-    
     main_loop = urwid.MainLoop(view.body, view.palette)
     try:
         main_loop.run()
     except KeyboardInterrupt:
         pass
 
-def register_events():
-    urwid.connect_signal(view.b_game, 'click', login)
-    urwid.connect_signal(view.b_quit, 'click', quit)
-    urwid.connect_signal(view.b_enter, 'click', enter)
-    urwid.connect_signal(view.b_start, 'click', start)
-
-def login(_button):
-    view.body.next()
-
-def quit(_button):
-    raise urwid.ExitMainLoop()
-
-def enter(_button):
-    model.player.username = view.e_username.get_edit_text()
-    model.game.init_score(model.player)
-
-    view.body.append(view.m_home(
-        model.player.username,
-        model.player.score,
-        model.player.new()))
-
-    view.body.next()
-
-def start(_button):
-    model.game.word = utils.random_word()
-
-    view.body.append(view.s_game(
+def display():
+    view.body.open(view.s_game(
         model.game.display,
         model.player,
         model.game.drawing,
         model.game.wrongs))
 
-    view.body.next()
+@eventhandler(view.b_game)
+def login(_button):
+    view.menu.next()
+
+@eventhandler(view.b_quit)
+def quit(_button):
+    raise urwid.ExitMainLoop()
+
+@eventhandler(view.b_enter)
+def enter(_button):
+    model.player.username = view.e_username.get_edit_text()
+    model.game.init_score(model.player)
+
+    view.body.open(view.m_home(
+        model.player,
+        model.player.new()))
+
+@eventhandler(view.b_start)
+def start(_button):
+    model.game.word = utils.random_word()
+    model.game.display = '*' * len(model.game.word)
+    display()
+
+@eventhandler(view.b_game_ok)
+def update(_button):
+    letter = view.e_letter.get_edit_text()[0].upper()
+    view.e_letter.set_edit_text('')
+
+    if letter in model.game.word:
+        model.game.update_display(letter)
+    else:
+        model.game.wrongs.append(letter)
+        model.game.lives -= 1
+
+    display()
