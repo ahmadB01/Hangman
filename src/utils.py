5fb29c4d62f3113312b80bb639fd9504503a5376
Ahmad <ahmad.baa003@gmail.com>
2019-06-28 19:48:29 +0200

    Path correction

--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,18 @@ import json
 import os
 
 def random_style():
-    path = '/home/ahmad/workspace/Hangman/src/titles/'
+    path = 'src/titles/'
     n = random.randint(0, len(os.listdir(path))-1)
     with open(path+str(n), 'r') as tf:
         return tf.read()
 
 def random_word():
-    path = '/home/ahmad/workspace/Hangman/src/data/words.json'
+    path = 'src/data/words.json'
     with open(path, 'r') as wf:
         words = json.loads(wf.read())
         return random.choice(words).upper()
 
 def drawings():
-    path = '/home/ahmad/workspace/Hangman/src/data/drawings.json'
+    path = 'src/data/drawings.json'
     with open(path, 'r') as df:
         return json.loads(df.read())
